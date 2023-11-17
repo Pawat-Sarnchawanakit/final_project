2af2ed28c5b41fce240f27d7b42ac41d62d9e1f6
Pawat Sarnchawanakit <pawat.sar@ku.th>
2023-11-17 20:57:06 +0700

    Idk if this is correct, I don't understand the instructions.

--- database.py
+++ database.py
@@ -1,19 +1,29 @@
 # try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
 
-import csv, os
+import csv
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+def readCsv(path, objType, callback):
+    with open(path) as f:
+        rows = csv.DictReader(f)
+        for r in rows:
+            obj = objType()
+            for i, v in r.items():
+                setattr(obj, i, v);
+            callback(obj);
 
-persons = []
-with open(os.path.join(__location__, 'persons.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        persons.append(dict(r))
-print(persons)
+
 
 # add in code for a Database class
-
+class Database:
+    def __init__(self):
+        self.data = {};
+    def get(self, key, default=None):
+        return self.data.get(key, default);
+    def put(self, key, val):
+        self.data[key] = val;
+    def forEach(self, callback):
+        for i, v in self.data.items():
+            callback(i, v);
 # add in code for a Table class
 
 # modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
--- project_manage.py
+++ project_manage.py
@@ -1,5 +1,41 @@
-# import database module
+import database, secrets;
+from hashlib import sha256
+mainDatabase = database.Database();
+peopleDatabase = database.Database();
+mainDatabase.put("people", peopleDatabase);
+loginDatabase = database.Database();
+mainDatabase.put("login", loginDatabase);
 
+class Person:
+    def __init__(self):
+        pass
+class LoginEntry:
+    def __init__(self, person_id, password, role):
+        self.person_id, self.password, self.role = person_id, password, role;
+
+class Roles:
+    Member = 0
+    Lead = 1
+    Faculty = 2
+    Advisor = 3
+    Admin = 4
+    def toString(role):
+        return ["Member", "Lead", "Faculty", "Advisor", "Admin"][role];
+
+abw = ''.join([chr(32+secrets.randbelow(95)) for _ in range(4)]);
+print(abw)
+database.readCsv("./persons.csv", Person, lambda personData: peopleDatabase.put(personData.ID, personData));
+peopleDatabase.forEach(lambda id, person: loginDatabase.put(f"{person.fist}.{person.last}", LoginEntry(id, sha256((abw+"42069").encode()).digest(), {'admin': Roles.Admin, 'student': Roles.Member, 'faculty': Roles.Faculty}[person.type])))
+
+def login():
+    username = input("Username: ")
+    password = sha256((input("Password: ")+"42069").encode()).digest()
+    entry = loginDatabase.get(username);
+    if entry == None or entry.password != password:
+        return;
+    print(entry.person_id, Roles.toString(entry.role))
+login()
+    #entry.password
 # start by adding the admin related code
 
 # create an object to read an input csv file, persons.csv
