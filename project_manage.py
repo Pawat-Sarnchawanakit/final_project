3333b4a0f545127248f80dcc50f28b609b2ed641
Pawat Sarnchawanakit <pawat.sar@ku.th>
2023-11-20 21:23:12 +0700

    Formatting, simple lead panel

--- project_manage.py
+++ project_manage.py
@@ -1,80 +1,97 @@
-from database import Database, CsvFile;
+from database import Database, CsvFile
 from hashlib import sha256
 import secrets, json
+
+
 class Role:
     Member = 0
     Lead = 1
     Faculty = 2
     Advisor = 3
     Admin = 4
+
     def toString(role):
-        return ["Member", "Lead", "Faculty", "Advisor", "Admin"][role];
+        return ["Member", "Lead", "Faculty", "Advisor", "Admin"][role]
+
 
 class ManageApp:
+
     def __init__(self):
-        self.mainDatabase = Database();
-        self.peopleTable = self.mainDatabase.addTable("people");
-        self.peopleTable.fromCsv("ID", CsvFile("./persons.csv"));
-        self.loginTable = self.mainDatabase.addTable("login");
-        self.peopleTable.forEach(lambda id, entry: self.loginTable.put(f"{entry['first']}.{entry['last'][0]}", {
-            "person_id": id,
-            "username": f"{entry['first']}.{entry['last'][0]}",
-            "password": (lambda passwd, salt: salt + sha256((passwd+salt).encode()).hexdigest())(''.join(chr(0x20) for _ in range(4)), ''.join(chr(0x20 + secrets.randbelow(95)) for _ in range(4))), # + secrets.randbelow(95)
-            "role": {
-                "student": Role.Member,
-                "faculty": Role.Faculty,
-                "admin": Role.Admin,
-            }[entry["type"]]
-        }));
-        self.projectsTable = self.mainDatabase.addTable("projects");
-        self.documentsTable = self.mainDatabase.addTable("documents");
+        self.mainDatabase = Database()
+        self.peopleTable = self.mainDatabase.addTable("people")
+        self.peopleTable.fromCsv("ID", CsvFile("./persons.csv"))
+        self.loginTable = self.mainDatabase.addTable("login")
+        self.peopleTable.forEach(lambda id, entry: self.loginTable.put(
+            f"{entry['first']}.{entry['last'][0]}",
+            {
+                "person_id":
+                id,
+                "username":
+                f"{entry['first']}.{entry['last'][0]}",
+                "password": (lambda passwd, salt: salt + sha256(
+                    (passwd + salt).encode()).hexdigest())
+                (''.join(chr(0x20) for _ in range(4)), ''.join(
+                    chr(0x20 + secrets.randbelow(95))
+                    for _ in range(4))),  # + secrets.randbelow(95)
+                "role": {
+                    "student": Role.Member,
+                    "faculty": Role.Faculty,
+                    "admin": Role.Admin,
+                }[entry["type"]]
+            }))
+        self.projectsTable = self.mainDatabase.addTable("projects")
+        self.documentsTable = self.mainDatabase.addTable("documents")
+
     def login(self):
-        username = input("Please login\nUsername: ");
-        password = input("Password: ");
-        loginEntry = self.loginTable.get(username);
+        username = input("Please login\nUsername: ")
+        password = input("Password: ")
+        loginEntry = self.loginTable.get(username)
         if loginEntry == None:
-            return;
-        password = sha256((password+loginEntry["password"][0:4]).encode()).hexdigest();
+            return
+        password = sha256(
+            (password + loginEntry["password"][0:4]).encode()).hexdigest()
         if loginEntry["password"][4:] != password:
-            return;
-        return loginEntry;
-    def adminPanel(self,_=None):
-        cur = self.mainDatabase;
+            return
+        return loginEntry
+
+    def adminPanel(self, _=None):
+        cur = self.mainDatabase
         curStr = '/'
         for cmd in iter(lambda: input("$ "), "exit"):
             if cmd == "ls":
-                print(cur.getData());
-                continue;
+                print(cur.getData())
+                continue
             if cmd == "cd":
-                toCdInto = cur.get(input("Enter key: "));
+                toCdInto = cur.get(input("Enter key: "))
                 if toCdInto == None:
                     print("Entry doesn't exist.")
-                    continue;
-                curStr += f"{toCdInto}/";
-                cur = toCdInto;
-                continue;
+                    continue
+                curStr += f"{toCdInto}/"
+                cur = toCdInto
+                continue
             if cmd == "home":
-                cur = self.mainDatabase;
-                continue;
+                cur = self.mainDatabase
+                continue
             if cmd == "set":
                 try:
-                    cur.put(input("Enter key: "), json.loads(input("Enter value: ")))
+                    cur.put(input("Enter key: "),
+                            json.loads(input("Enter value: ")))
                 except:
-                    print("Bad value.");
-                continue;
+                    print("Bad value.")
+                continue
             if cmd == "get":
                 entry = cur.get(input("Enter key: "))
                 if entry == None:
                     print("Invalid key.")
-                    continue;
+                    continue
                 try:
                     print(json.dumps(entry))
                 except:
-                    print(entry);
-                continue;
+                    print(entry)
+                continue
             if cmd == "delete":
                 cur.delete(input("Enter key: "))
-                continue;
+                continue
             if cmd == "help":
                 print(
                     "ls - list all entries\n" \
@@ -85,14 +102,15 @@ class ManageApp:
                     "delete - deletes an entry\n"
                     "help - display this"
                     )
-                continue;
+                continue
             print("Unknown command!\nType \"help\" for a list of commands.")
+
     def sendMsg(self, data, targetId):
         targetData = self.peopleTable.get(targetId)
-        reqs = targetData.get("requests");
+        reqs = targetData.get("requests")
         if reqs == None:
-            reqs = [];
-            targetData["requests"] = reqs;
+            reqs = []
+            targetData["requests"] = reqs
         reqs.append({
             "title": input("Enter title: "),
             "content": input("Enter message: "),
@@ -101,46 +119,51 @@ class ManageApp:
                 "id": data["ID"]
             }
         })
+
     def requestPanel(self, data, req):
         print(f"Title: {req['title']}\n\n{req['content']}")
-        for cmd in iter(lambda: input(
-            "0. Go back\n"
-            "1. Reply\n"
-            "2. Reply and delete\n"
-            "3. Delete\nChoose: "), "0"):
+        for cmd in iter(
+                lambda: input("0. Go back\n"
+                              "1. Reply\n"
+                              "2. Reply and delete\n"
+                              "3. Delete\nChoose: "), "0"):
             if cmd == '1':
                 self.sendMsg(data, req['sender']['id'])
-                continue;
+                continue
             if cmd == '2':
                 self.sendMsg(data, req['sender']['id'])
-                return True;
-                continue;
+                return True
+                continue
             if cmd == '3':
-                return True;
+                return True
+
     def requestsPanel(self, data):
-        reqs = data.get("requests", []);
+        reqs = data.get("requests", [])
         while True:
             if not reqs:
                 print("There are no requests at the moment.")
-                return;
+                return
             print("List of requests: ")
-            idx = 0;
+            idx = 0
             for req in reqs:
-                print(f"{idx}. {req['title']} - {req['sender']['name']}");
-                idx += 1;
-            cmd = input("Choose a request you want to view, type \"exit\" to exit.\nRequest: ");
+                print(f"{idx}. {req['title']} - {req['sender']['name']}")
+                idx += 1
+            cmd = input(
+                "Choose a request you want to view, type \"exit\" to exit.\nRequest: "
+            )
             if cmd == "exit":
-                break;
+                break
             try:
-                idx = int(cmd);
+                idx = int(cmd)
                 if idx >= len(reqs):
                     print("Index out of bounds.")
-                    continue;
+                    continue
                 if self.requestPanel(data, req):
-                    reqs[idx] = reqs[::-1][0];
-                    reqs.pop();
+                    reqs[idx] = reqs[::-1][0]
+                    reqs.pop()
             except:
                 pass
+
     def allProjectsPanel(self, data):
         for cmd in iter(lambda: input(
             "What do you want to do?\n" \
@@ -151,42 +174,43 @@ class ManageApp:
             "4. View project info\n\nChoose: " \
             ), '0'):
             if cmd == '1':
-                items = self.projectsTable.getData().items();
+                items = self.projectsTable.getData().items()
                 if not items:
                     print("There are no projects.")
-                    continue;
+                    continue
                 for i, v in items:
-                    print(i, v);
-                continue;
+                    print(i, v)
+                continue
             if cmd == '2':
                 project = self.projectsTable.get(input("Enter project id: "))
                 if project == None:
                     print("Invalid project id.")
-                    continue;
-                curAdvisor = project["advisor"];
+                    continue
+                curAdvisor = project["advisor"]
                 if curAdvisor != None and input(
                     "There is already an advisor for this project.\n" \
                     "Do you want to overwrite this? (y/n) "
                     ) != 'y':
-                    continue;
+                    continue
                 curAdvisor = {
                     "name": f"{data['first']} {data['last']}",
                     "id": data["ID"]
-                };
-                project["advisor"] = curAdvisor;
-                continue;
+                }
+                project["advisor"] = curAdvisor
+                continue
             if cmd == '3':
                 project = self.projectsTable.get(input("Enter project id: "))
                 if project == None:
                     print("Invalid project id.")
-                    continue;
-                project["approved"] = True;
+                    continue
+                project["approved"] = True
             if cmd == '4':
                 project = self.projectsTable.get(input("Enter project id: "))
                 if project == None:
                     print("Invalid project id.")
-                    continue;
+                    continue
                 print(project)
+
     def facultyPanel(self, data):
         for cmd in iter(lambda: input(
             "What do you want to do?\n" \
@@ -195,17 +219,67 @@ class ManageApp:
             "2. View projects\n\nChoose: " \
             ), '0'):
             if cmd == '1':
-                self.requestsPanel(data);
-                continue;
+                self.requestsPanel(data)
+                continue
             if cmd == '2':
-                self.allProjectsPanel(data);
+                self.allProjectsPanel(data)
+                continue
+
+    def getUniqueProjectId(self):
+        while True:
+            id = secrets.token_hex(16)
+            if self.projectsTable.get(id) != None:
+                return id
+
+    def leadPanel(self, data):
+        for cmd in iter(lambda: input(
+            "What do you want to do?\n" \
+            "0. Exit\n" \
+            "1. Create a project\n" \
+            "2. View all available members\n" \
+            "3. Send Message\n" \
+            "4. View personal projects\n\nChoose: " \
+            ), '0'):
+            if cmd == '1':
+                projs = data.get("projects")
+                if projs == None:
+                    projs = []
+                    data["projects"] = projs
+                projId = self.getUniqueProjectId()
+                newProj = {
+                    "name":
+                    input("Enter project name: "),
+                    "description":
+                    input("Enter description: "),
+                    "id":
+                    projId,
+                    "members": [{
+                        "name": f"{data['first']} {data['last']}",
+                        "id": data['ID']
+                    }]
+                }
+                projs.append(newProj)
+                continue
+            if cmd == '2':
+                for v in self.loginTable.getData().values():
+                    if v["role"] != Role.Member:
+                        continue;
+                    
                 continue;
+            if cmd == '3':
+                self.sendMsg(data, input("Enter target id: "))
+                continue
+
     def run(self):
         while True:
-            info = self.login();
+            info = self.login()
             if info == None:
-                print("Invalid credentials, please try again.");
-                continue;
-            [lambda: None, lambda: None, self.facultyPanel, self.facultyPanel, self.adminPanel][info["role"]](self.peopleTable.get(info["person_id"]));
+                print("Invalid credentials, please try again.")
+                continue
+            [
+                lambda: None, self.leadPanel, self.facultyPanel,
+                self.facultyPanel, self.adminPanel
+            ][info["role"]](self.peopleTable.get(info["person_id"]))
 
-ManageApp().run();
+
+ManageApp().run()
